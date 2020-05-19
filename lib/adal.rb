86808791027edaebf4304fd2984195f42ea65c4d
ZengTao <taozeng@mindhand.io>
2020-05-19 16:22:57 +0800

    [修复] Gem 加载的修复

--- lib/adal.rb
+++ lib/adal.rb
@@ -21,4 +21,9 @@
 #-------------------------------------------------------------------------------
 
 # Extract all Ruby files in $DIR/lib/adal/ regardless of where the gem is built.
-Dir[File.expand_path('../adal/*.rb', __FILE__)].each { |f| require_relative f }
+
+require_relative './adal/request_parameters.rb'
+
+Dir[File.expand_path('../adal/*.rb', __FILE__)].each do |f|  
+  require_relative f 
+end
